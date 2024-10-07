6fa84378b044084eef5b3e270b2e348aa670a4a3
minhye <doreminye@gmail.com>
2024-10-07 11:01:07 +0900

    비번, 닉네임, 전번 수정 기능 추가

--- duks_fe/src/pages/MyPage.js
+++ duks_fe/src/pages/MyPage.js
@@ -30,7 +30,26 @@ const MyPage = () => {
   const closeLogoutModal = () => setLogoutModalOpen(false);
   const openDeleteModal = () => setDeleteModalOpen(true);
   const closeDeleteModal = () => setDeleteModalOpen(false);
-  
+
+  // 수정 가능한 상태 관리
+  const [password, setPassword] = useState('***********');
+  const [phone, setPhone] = useState('010-1234-0382');
+  const [nickname, setNickname] = useState('덕성오리');
+
+  // 수정 모드 관리
+  const [isPwdEditable, setPwdEditable] = useState(false);
+  const [isPhoneEditable, setPhoneEditable] = useState(false);
+  const [isNickEditable, setNickEditable] = useState(false);
+
+  // 더블클릭 이벤트 핸들러
+  const handleDoubleClickPwd = () => setPwdEditable(true);
+  const handleDoubleClickPhone = () => setPhoneEditable(true);
+  const handleDoubleClickNick = () => setNickEditable(true);
+
+  // 수정 완료 핸들러
+  const handleBlurPwd = () => setPwdEditable(false);
+  const handleBlurPhone = () => setPhoneEditable(false);
+  const handleBlurNick = () => setNickEditable(false);
 
   return (
     <BackgroundWrapper>
@@ -47,16 +66,53 @@ const MyPage = () => {
               <Phone>휴대전화</Phone>
 
               <StdNumText>20221234</StdNumText>
-              <PwdText>***********</PwdText>
-              <NickText>덕성오리</NickText>
+
+              {/* 비밀번호 수정 가능하게 */}
+              {isPwdEditable ? (
+                <input 
+                  type="text" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                  onBlur={handleBlurPwd}
+                  autoFocus
+                />
+              ) : (
+                <PwdText onDoubleClick={handleDoubleClickPwd}>{password}</PwdText>
+              )}
+
+              {/* 닉네임 수정 가능하게 */}
+              {isNickEditable ? (
+                <input 
+                  type="text" 
+                  value={nickname} 
+                  onChange={(e) => setNickname(e.target.value)} 
+                  onBlur={handleBlurNick}
+                  autoFocus
+                />
+              ) : (
+                <NickText onDoubleClick={handleDoubleClickNick}>{nickname}</NickText>
+              )}
+
               <DepartText>IT</DepartText>
-              <PhoneText>010-1234-0382</PhoneText>
+
+              {/* 휴대전화 수정 가능하게 */}
+              {isPhoneEditable ? (
+                <input 
+                  type="text" 
+                  value={phone} 
+                  onChange={(e) => setPhone(e.target.value)} 
+                  onBlur={handleBlurPhone}
+                  autoFocus
+                />
+              ) : (
+                <PhoneText onDoubleClick={handleDoubleClickPhone}>{phone}</PhoneText>
+              )}
             </OverlapGroup>
           </Person>
-          <TextWrapper>덕성오리 님
-          <ImageUploader 
-                defaultImage={require('../img/duk_img.png')} // 기본 이미지 경로
-                className="myPageProfileImage" // 추가 클래스 이름 (스타일 지정 시 활용)
+          <TextWrapper>{nickname} 님
+            <ImageUploader 
+              defaultImage={require('../img/duk_img.png')} // 기본 이미지 경로
+              className="myPageProfileImage" // 추가 클래스 이름 (스타일 지정 시 활용)
             />
           </TextWrapper>
 
